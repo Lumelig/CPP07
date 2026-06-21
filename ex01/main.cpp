c7308f358bc95098e2eb334349bb89791c2dcbb4
lumelig <pfleghaar.j@gmail.com>
2026-06-21 12:37:21 +0200

    started ex02

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Jpflegha <jpflegha@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 15:41:39 by Jpflegha          #+#    #+#             */
-/*   Updated: 2026/06/16 15:47:17 by Jpflegha         ###   ########.fr       */
+/*   Updated: 2026/06/20 14:04:15 by Jpflegha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,11 @@ int main() {
     int numbers[] = {1, 2, 3, 4, 5};
     std::size_t n = sizeof(numbers) / sizeof(numbers[0]);
 
-    std::cout << "--- print all numbers from the arry ---:" << std::endl;
     iter(numbers, n, printElem<int>);   // print each
     iter(numbers, n, addOne<int>);      // add one to each
-    std::cout << "--- added one to each number ---" << std::endl;
     iter(numbers, n, printElem<int>);   // print again — values changed
+
     std::string words[] = {"hello", "world"};
-    std::cout << "--- print all chars from the arry ---" << std::endl;
     iter(words, 2, printElem<std::string>);
     return 0;
 }
